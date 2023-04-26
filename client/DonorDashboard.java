18cfe10ec634d9016269122e2fbd8a8a7d27f1cb
faysalahammedchowdhury01 <faysalahammedchowdhury01@gmail.com>
2023-04-26 13:09:47 +0600

    added

--- client/DonorDashboard.java
+++ client/DonorDashboard.java
@@ -12,34 +12,151 @@ import java.awt.*;
 import server.classes.*;
 
 public class DonorDashboard {
+    private boolean isShowDropdown;
+
+    JFrame frame;
     private JPanel mainPanel, navbarPanel, postPanel;
+    private JScrollPane scrollPane;
+    private ImageIcon favIcon, icon;
+    // navbar
+    private BufferedImage image;
+    private JLabel aiubLogo;
+    private JLabel logo;
+    private JLabel aiubText;
+    private JButton name;
+    private JLabel dropdownBox;
+    private JButton seeProfile;
+    private JButton myDonationsButton;
+    private JButton donorsListButton;
+    private JButton logoutButton;
 
     public DonorDashboard(Donor d) {
-        // Create a JPanel for the navigation bar
-        navbarPanel = new JPanel();
-        // navbarPanel.setPreferredSize(new Dimension(400, 50));
-        navbarPanel.setBackground(Color.BLUE);
-        JLabel navLabel = new JLabel("Navigation Bar");
-        navbarPanel.add(navLabel);
+        frame = new JFrame("Dashboard - AIUB BLOOD DONATIIN CLUB");
 
-        // Create a JPanel for the posts
-        postPanel = new JPanel();
-        postPanel.setLayout(new BoxLayout(postPanel, BoxLayout.Y_AXIS));
-        for (Post post : Post.posts) {
-            PostPanel panelPost = new PostPanel(post);
-            postPanel.add(panelPost);
+        // favIcon
+        favIcon = new ImageIcon("images/logo.png");
+        frame.setIconImage(favIcon.getImage());
+
+        // navbar panel
+        navbarPanel = new JPanel();
+        navbarPanel.setLayout(null);
+        navbarPanel.setPreferredSize(new Dimension(1366, 80));
+        navbarPanel.setBackground(new Color(169, 29, 20));
+
+        // aiub and blood logo
+        try {
+            image = ImageIO.read(new File("images/aiub_logo_sm.png"));
+            aiubLogo = new JLabel(new ImageIcon(image));
+            aiubLogo.setBounds(50, 15, image.getWidth(), image.getHeight());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        // Create a main panel to hold the navigation bar and post panel
+        try {
+            image = ImageIO.read(new File("images/logo_sm.png"));
+            logo = new JLabel(new ImageIcon(image));
+            logo.setBounds(100, 15, image.getWidth(), image.getHeight());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // aiub text
+        aiubText = new JLabel("AIUB BLOOD DONATION CLUB");
+        aiubText.setBounds(160, 15, 400, 50);
+        aiubText.setForeground(Color.WHITE);
+        aiubText.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 22));
+
+        // name label and dropdown
+        icon = new ImageIcon("images/dropdown.png");
+        name = new JButton("Welcome, " + d.getName().split(" ")[0], icon);
+        name.setBounds(1366 - 320, 15, 250, 50);
+        name.setForeground(Color.WHITE);
+        name.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 18));
+        name.setOpaque(false);
+        name.setContentAreaFilled(false);
+        name.setBorderPainted(false);
+        name.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+
+        // dropdown box
+        dropdownBox = new JLabel("");
+        dropdownBox.setBounds(1366 - 300, 70, 250, 340);
+        dropdownBox.setBackground(Color.GRAY);
+        dropdownBox.setOpaque(true);
+        dropdownBox.setVisible(false);
+        isShowDropdown = false;
+
+        // see profile button
+        seeProfile = new JButton("See Profile");
+        seeProfile.setBounds(1366 - 280, 80, 210, 65);
+        seeProfile.setBackground(Color.WHITE);
+        seeProfile.setForeground(Color.BLACK);
+        seeProfile.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 18));
+        seeProfile.setBorderPainted(false);
+        seeProfile.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        seeProfile.setVisible(false);
+
+        // my donations button
+        myDonationsButton = new JButton("My Donations");
+        myDonationsButton.setBounds(1366 - 280, 160, 210, 65);
+        myDonationsButton.setBackground(Color.WHITE);
+        myDonationsButton.setForeground(Color.BLACK);
+        myDonationsButton.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 18));
+        myDonationsButton.setBorderPainted(false);
+        myDonationsButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        myDonationsButton.setVisible(false);
+
+        // donors list button
+        donorsListButton = new JButton("Donors List");
+        donorsListButton.setBounds(1366 - 280, 240, 210, 65);
+        donorsListButton.setBackground(Color.WHITE);
+        donorsListButton.setForeground(Color.BLACK);
+        donorsListButton.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 18));
+        donorsListButton.setBorderPainted(false);
+        donorsListButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        donorsListButton.setVisible(false);
+
+        // logout button
+        logoutButton = new JButton("Logout");
+        logoutButton.setBounds(1366 - 280, 320, 210, 65);
+        logoutButton.setBackground(Color.WHITE);
+        logoutButton.setForeground(Color.BLACK);
+        logoutButton.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 18));
+        logoutButton.setBorderPainted(false);
+        logoutButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        logoutButton.setVisible(false);
+
+        // navbar adding
+        navbarPanel.add(aiubLogo);
+        navbarPanel.add(logo);
+        navbarPanel.add(aiubText);
+        navbarPanel.add(name);
+        frame.add(seeProfile);
+        frame.add(myDonationsButton);
+        frame.add(donorsListButton);
+        frame.add(logoutButton);
+        frame.add(dropdownBox);
+
+        // post panel
+        postPanel = new JPanel();
+        postPanel.setLayout(new BoxLayout(postPanel, BoxLayout.Y_AXIS));
+        postPanel.setBackground(Color.red);
+
+        // posts
+        for (Post post : Post.posts) {
+            PostGUI singlePost = new PostGUI(post);
+            postPanel.add(singlePost);
+        }
+
+        // main panel
         mainPanel = new JPanel(new BorderLayout());
         mainPanel.add(navbarPanel, BorderLayout.NORTH);
         mainPanel.add(postPanel, BorderLayout.CENTER);
 
-        // Create a JScrollPane and add the main panel to it
-        JScrollPane scrollPane = new JScrollPane(mainPanel);
+        // scroll pane
+        scrollPane = new JScrollPane(mainPanel);
 
-        // Create a JFrame and add the JScrollPane to it
-        JFrame frame = new JFrame("Post Scroll Pane Example");
+        // adding to frame
+        frame.setIconImage(favIcon.getImage());
         frame.add(scrollPane);
 
         // frame
@@ -48,19 +165,46 @@ public class DonorDashboard {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
+        // action listeners
+
+        // dropdown action
+        name.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (isShowDropdown) {
+                    hideDropdown();
+                } else {
+                    showDropdown();
+                }
+            }
+        });
+
+        // logout action
+        logoutButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                new Login();
+            }
+        });
     }
 
-    private static class PostPanel extends JPanel {
-        public PostPanel(Post post) {
-            setLayout(new GridLayout(2, 3));
-            JLabel dateLabel = new JLabel("Date: " + post.getDate());
-            JLabel locationLabel = new JLabel("Location: " + post.getLocation());
-            JLabel descriptionLabel = new JLabel("Description: " + post.getDescription());
-            add(dateLabel);
-            add(locationLabel);
-            add(descriptionLabel);
-            setBorder(BorderFactory.createLineBorder(Color.BLACK));
-            setPreferredSize(new Dimension(200, 100));
-        }
+    // show dropwdown
+    private void showDropdown() {
+        isShowDropdown = true;
+        dropdownBox.setVisible(true);
+        seeProfile.setVisible(true);
+        myDonationsButton.setVisible(true);
+        donorsListButton.setVisible(true);
+        logoutButton.setVisible(true);
+    }
+
+    // hide dropdown
+    private void hideDropdown() {
+        isShowDropdown = false;
+        dropdownBox.setVisible(false);
+        seeProfile.setVisible(false);
+        myDonationsButton.setVisible(false);
+        donorsListButton.setVisible(false);
+        logoutButton.setVisible(false);
     }
 }
